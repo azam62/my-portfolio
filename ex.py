7c0886c6e7169f4f26a31ec9edde5e71672117b0
azam62 <164209875+azam62@users.noreply.github.com>
2024-03-20 22:30:27 +0530

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,16 @@
+x:integer --global
+procedure set_x(n:integer)
+x:=n
+procedure print_x
+write_integer (x)
+procudure first
+set_x(1)
+print_x
+procedure second 
+x:=integer
+set_x(0)
+first()
+print_x
+second ()
+print_x
+
